35fdd0af7d8568aa2ea3f14d53c18265c93a7c1a
William <wrassis84@gmail.com>
2024-06-01 15:28:36 -0300

    docs: upload files

---
+++ deploy.sh
@@ -0,0 +1,40 @@
+#### ENVIRONMENT         #######################################################
+
+#!/usr/bin/env bash
+
+#### ABOUT               #######################################################
+
+# Repository      : https://github.com/wrassis84/infra_files.git
+# Author          : William Ramos de Assis Rezende
+# Maintainer      : William Ramos de Assis Rezende
+#
+# DataTex.sh      : List, Add and Remove users from DataTex systems.
+# Requirements    : LibTex.sh
+# Usage           : DataTex.sh [ list | add | remove ]
+
+#### TESTING ENVIRONMENT #######################################################
+#
+# GNU bash, version 5.2.21
+#
+#### TESTS/VALIDATIONS   #######################################################
+
+#### VARIABLES           #######################################################
+#
+$repo="https://github.com/wrassis84/infra_files.git"
+#
+#### FUNCTIONS           #######################################################
+
+#### MAIN CODE           #######################################################
+#
+cd ~
+git clone $repo
+cp infra_files/.bashrc $HOME/.bashrc
+cp infra_files/.bashrc_root /root/.bashrc
+cp infra_files/.vimrc $HOME/.vimrc /root/.vimrc
+cp infra_files/issue /etc/issue
+cp infra_files/issue.net /etc/issue.net
+cp infra_files/motd /etc/motd
+cp infra_files/sshd_config /etc/ssh/sshd_config
+#
+#### MAIN CODE           #######################################################
+# TODO: Check execution permissions for this script file itself.
